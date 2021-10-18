502e9abb605f1f9bb187e0ae207c004be354895f
Ryoneme <75056575+Ryonem2@users.noreply.github.com>
2021-10-18 12:49:45 +0700

    add modal and modal activated

--- main.js
+++ main.js
@@ -13,3 +13,29 @@ for (let i = 0; i < eduList.length; i++) {
     }
   });
 }
+
+const boxInfo = document.querySelectorAll(".box-info2");
+const modal = document.querySelectorAll(".modal-bg");
+
+for (let i = 0; i < boxInfo.length; i++) {
+  boxInfo[i].addEventListener("click", () => {
+    modal[i].style.display = "block";
+    for (let j = 0; j < boxInfo.length; j++) {
+      if (j == i) {
+        continue;
+      } else {
+        modal[j].style.display = "none";
+      }
+    }
+  });
+}
+
+const close = document.querySelectorAll(".closed");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].addEventListener("click", () => {
+    for (let j = 0; j < close.length; j++) {
+      modal[j].style.display = "none";
+    }
+  });
+}
