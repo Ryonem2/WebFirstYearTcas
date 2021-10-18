8d047c38a1d4936d8397e17fbc5c1fcab592f460
Ryoneme <75056575+Ryonem2@users.noreply.github.com>
2021-10-18 14:25:27 +0700

    add f scrollInToView

--- main.js
+++ main.js
@@ -46,11 +46,14 @@ for (let i = 0; i < close.length; i++) {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+}
+
+function ScrollToTarget(id) {
+  let target = document.querySelector(id);
+  target.scrollIntoView(true);
 }
